7b76acf2e393078349785f79b2a6cdecf08ca5bf
tunacane <tunacaneski@gmail.com>
2024-08-25 21:44:36 +0300

    Soru2 harf büyütme küçültme sorusu tamamlandı

---
+++ 01-String-Methods-Homework/Soru2/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Soru2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string sentence = "Birinci sorudaki kelime sayısını bulunuz";
+
+        System.Console.Write($"Büyük harf: {sentence.ToUpper()}\nKüçük harf: {sentence.ToLower()}");
+       
+    }
+}
