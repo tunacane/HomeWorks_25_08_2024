eb973484a38ffd4edd051ad2325f27c50c2ae214
tunacane <tunacaneski@gmail.com>
2024-08-25 21:36:19 +0300

    Soru1 split ile kelime sayısı bulma yapıldı.

---
+++ 01-String-Methods-Homework/Soru1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Soru1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       string cumle1 = "Birinci sorudaki kelime sayısını bulunuz";
+       string[] word = cumle1.Split(" "); 
+       
+       int wordCount = word.Length;
+       System.Console.WriteLine(wordCount);
+    }
+}
