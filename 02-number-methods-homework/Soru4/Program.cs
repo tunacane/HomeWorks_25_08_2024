c6f0cf0a391806554b558a0e1b8dc427b3291f75
tunacane <tunacaneski@gmail.com>
2024-08-27 15:38:41 +0300

    soru4 tamamlandı

---
+++ 02-number-methods-homework/Soru4/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Soru4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Bir sayı giriniz: ");
+        double number1 = double.Parse(Console.ReadLine());
+
+        System.Console.WriteLine(Math.Sqrt(number1));
+    }
+}
