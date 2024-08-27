6ba66c2ab55ccb22bbd426b00a186674aa9830b5
tunacane <tunacaneski@gmail.com>
2024-08-27 16:47:04 +0300

    soru8 tamamlandı

---
+++ 02-number-methods-homework/Soru8/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Soru8;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Bir sayı giriniz: ");
+
+        double number = Convert.ToDouble(Console.ReadLine());
+
+        double numberLog = Math.Log(number);
+
+        System.Console.WriteLine(numberLog);
+        
+    }
+}
---
+++ 02-number-methods-homework/Soru9,/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Soru9_;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
