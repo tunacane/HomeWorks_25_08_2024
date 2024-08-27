ef0bbfdad047a951dab77dcf9765b59a3efeecb1
tunacane <tunacaneski@gmail.com>
2024-08-27 15:34:10 +0300

    soru3 tamamlandı

---
+++ 02-number-methods-homework/Soru3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Xml.XPath;
+
+namespace Soru3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Bir sayı giriniz: ");
+        int number1 = Convert.ToInt32(Console.ReadLine());
+
+        int result = Math.Abs(number1);
+        System.Console.WriteLine(result);
+    }
+}
