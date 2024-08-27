0c923c068d7789d45cc7a0d1efb1fceebb828b5e
tunacane <tunacaneski@gmail.com>
2024-08-27 16:52:13 +0300

    soru9 tamamlandı

--- 02-number-methods-homework/Soru9,/Program.cs
+++ 02-number-methods-homework/Soru9,/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Birinci sayıyı giriniz: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("İkinci sayıyı giriniz: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        string result;
+
+        if (num1 > num2)
+        {
+            result = "birinci sayı büyüktür.";
+        }
+        else if (num2 > num1)
+        {
+            result = "ikinci sayı büyüktür.";
+        }
+        else{
+            result = "sayılar eşittir.";
+        }
+
+        System.Console.WriteLine(result);
     }
 }
