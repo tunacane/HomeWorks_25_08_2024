dc8d63d76524b0f2e1f0a8126bfea5fed23d5be1
tunacane <tunacaneski@gmail.com>
2024-08-28 12:41:05 +0300

    soru6 tamamlandı

---
+++ 03-datetime-methods-homework/Soru6/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Soru6;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Bir tarih giriniz: ");
+
+        int date = Convert.ToInt32(Console.ReadLine());
+
+        bool leapYear =  DateTime.IsLeapYear(date);
+
+        System.Console.WriteLine(leapYear);
+    }
+}
