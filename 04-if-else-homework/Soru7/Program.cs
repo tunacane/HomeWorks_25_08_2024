f29b5cd37df7f3e6871deff780c4c98b044af68e
tunacane <tunacaneski@gmail.com>
2024-08-28 21:17:46 +0300

    soru7 tamamlandı

---
+++ 04-if-else-homework/Soru7/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Soru7;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Birinci sayıyı giriniz: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("İkinci sayıyı giriniz: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Üçüncü sayıyı giriniz: ");
+        int num3 = Convert.ToInt32(Console.ReadLine());
+
+        int enBuyuk;
+
+        if (num1 > num2 && num1 > num3)
+        {
+            System.Console.WriteLine($"En büyük sayı: {num1}");
+        }
+        else if (num2 > num1 && num2 > num3)
+        {
+            System.Console.WriteLine($"En büyük sayı: {num2}");
+        }
+        else{
+            System.Console.WriteLine($"En büyük sayı: {num3}");
+        }
+    }
+}
