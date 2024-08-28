31a9e776eb8dfd69f3068aa669762746b4a243c6
tunacane <tunacaneski@gmail.com>
2024-08-28 13:54:57 +0300

    soru10 tamamlandı

---
+++ 03-datetime-methods-homework/Soru10/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http.Headers;
+
+namespace Soru10;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Bir tarih giriniz: ");
+
+        string time = Console.ReadLine();
+
+        DateTime date = Convert.ToDateTime(time);
+
+        System.Console.WriteLine(date.ToString("HH"));
+
+        // System.Console.WriteLine(now.ToString("HH:mm:ss"));
+
+
+    }
+}
