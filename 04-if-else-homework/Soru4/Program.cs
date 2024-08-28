332d397672870cf2fe7ca0b78b204d5ea435a616
tunacane <tunacaneski@gmail.com>
2024-08-28 17:09:57 +0300

    soru4 tamamlandı

---
+++ 04-if-else-homework/Soru4/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Soru4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+      System.Console.WriteLine("Bir tarih giriniz: ");
+      int year = Convert.ToInt32(Console.ReadLine());
+
+      bool IsLeapYear  = DateTime.IsLeapYear(year);
+     
+      if (IsLeapYear)
+      {
+        System.Console.WriteLine($"{year} artık yıldır.");
+      }
+      else{
+            System.Console.WriteLine($"{year} artık yıl değildir.");
+        }
+
+
+    }
+}
