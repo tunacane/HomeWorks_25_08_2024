e6763599dd86563e3ca6c131e9925a1bfbb0b934
tunacane <tunacaneski@gmail.com>
2024-08-28 21:25:49 +0300

    soru8 tamamlandı

---
+++ 04-if-else-homework/Soru8/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Soru8;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Bir sayı giriniz: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        if (num1 %3 ==0 && num1 %5 ==0)
+        {
+            System.Console.WriteLine(num1 +": 3 ve 5 ile kalansız bölünebilir.");
+        }
+        else
+        {
+            System.Console.WriteLine(num1+ ": 3 ve 5 ile kalansız bölünemez. ");
+        }
+    }
+}
