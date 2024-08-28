5d4b3f671be23df95cb0f8a6b28500381ecfdb97
tunacane <tunacaneski@gmail.com>
2024-08-28 12:26:08 +0300

    soru5 tamamlandı

---
+++ 03-datetime-methods-homework/Soru5/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Soru5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Bir tarih giriniz: ");
+        DateTime date = Convert.ToDateTime(Console.ReadLine());
+
+        int dayOfDate = date.DayOfYear;
+
+        System.Console.WriteLine(dayOfDate);
+
+
+    }
+}
