653d4004174ba4ebebf6b5d266053827850d5965
tunacane <tunacaneski@gmail.com>
2024-08-28 12:22:49 +0300

    soru4 tamamlandı

---
+++ 03-datetime-methods-homework/Soru4/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Soru4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DateTime date1 = new DateTime(1987 , 12 ,14);
+        DateTime date2 = new DateTime(1992, 11, 13);
+
+        TimeSpan newDate = (date2 - date1);
+
+    
+        System.Console.WriteLine(Math.Abs(newDate.TotalDays));
+
+    }
+}
