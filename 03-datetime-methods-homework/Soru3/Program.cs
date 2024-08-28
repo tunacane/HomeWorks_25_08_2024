21f4da2db0eaacc03bde57852494a27fdd90c91e
tunacane <tunacaneski@gmail.com>
2024-08-28 12:13:32 +0300

    soru3 tamamlandı

---
+++ 03-datetime-methods-homework/Soru3/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Soru3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Bir tarih giriniz: ");
+
+        DateTime date = Convert.ToDateTime(Console.ReadLine());
+
+        System.Console.WriteLine(date.AddDays(52));
+    }
+}
