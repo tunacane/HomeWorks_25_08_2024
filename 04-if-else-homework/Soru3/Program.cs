5614a70ea445939684711eef3b05d9397a10e330
tunacane <tunacaneski@gmail.com>
2024-08-28 16:55:44 +0300

    soru3 tamamlandı

---
+++ 04-if-else-homework/Soru3/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Soru3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Bir harf giriniz: ");
+        char letter = Convert.ToChar(Console.ReadLine());
+
+       bool isVowel =  (letter == 'a') || (letter =='e') || (letter =='ı') ||  (letter == 'i') ||  (letter == 'o' )|| (letter == 'ö' )||  (letter =='u' )||  (letter == 'ü');
+
+       if (isVowel)
+       {
+        System.Console.WriteLine($"{letter}  sesli bir harftir.");
+       }
+       else
+       {
+        System.Console.WriteLine($"{letter} sessiz bir harftir. ");
+       }
+
+
+
+    }
+}
