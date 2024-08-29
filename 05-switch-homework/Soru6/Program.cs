f3b79110d6ae7fd8e7f9b41c897070197f05e58c
tunacane <tunacaneski@gmail.com>
2024-08-29 18:33:31 +0300

    soru6 tamamlandı

---
+++ 05-switch-homework/Soru6/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Soru6;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Sınav notunuzu giriniz: ");
+        int point = Convert.ToInt32(Console.ReadLine());
+        string result;
+
+        if (point >=30 && point <= 100)
+        {
+            switch (point)
+            {
+                case < 45:
+                    result = "F";
+                    break;
+                case < 65:
+                    result = "D";
+                    break;
+                case < 75:
+                    result = "C";
+                    break;
+                case < 85:
+                    result = "B";
+                    break;
+                case < 100:
+                    result = "A";
+                    break;
+            
+                default:
+                    result = "Invlaid value";
+                    break;
+            }
+            System.Console.WriteLine($"Your point is {result}");
+        }
+        else
+        {
+            System.Console.WriteLine("You failed!");
+        }
+    }
+}
