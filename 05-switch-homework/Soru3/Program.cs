64103eb6c26c0be9e7b2f21d3e5c7db5df41b11b
tunacane <tunacaneski@gmail.com>
2024-08-29 16:12:59 +0300

    soru4 tamamlandı

---
+++ 05-switch-homework/Soru3/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Soru3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Birinci sayıyı giriniz: ");
+        double sayi1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("İkinci sayıyı giriniz: ");
+       double sayi2 = Convert.ToDouble(Console.ReadLine());
+
+        System.Console.WriteLine("Lütfen işlemi giriniz: ");
+        string islem = Console.ReadLine();
+
+        double toplam = sayi1 +sayi2;
+        double fark = sayi1 -sayi2;
+        double carpim = sayi1*sayi2;
+        double bolme = sayi1/sayi2;
+
+        double result =0;
+        
+        if (true)
+        {
+            switch (islem)
+            {
+                case "+":
+                    result= toplam;
+                    break;
+                case "-":
+                    result = fark;
+                    break;
+                case "*":
+                    result = carpim;
+                    break;
+
+                default:
+                    result = bolme;
+                    break;
+            }
+            System.Console.WriteLine($"{sayi1} {islem} {sayi2} = {result}");
+        }
+        else{
+            System.Console.WriteLine("Geçersiz veri girdiniz!");
+        }
+    }
+}
---
+++ 05-switch-homework/Soru4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Reflection.PortableExecutable;
+using System.Threading.Tasks.Dataflow;
+
+namespace Soru;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Bir harf giriniz: ");
+        string letter = Console.ReadLine().ToLower();
+
+        string result="";
+
+        if (letter.Length == 1 && char.IsLetter(letter[0]))
+        {
+
+            switch (letter)
+            {
+                case "a":
+                case "e":
+                case "ı" :                 
+                case "i":      
+                case "o":       
+                case "ö":    
+                case "u":       
+                case "ü":          
+                    result = "letter is vowel.";
+                    break;
+
+                default:
+
+                    result = "letter is not vowel";
+                    break;
+            }
+            System.Console.WriteLine(result);
+        }
+        else
+        {
+            System.Console.WriteLine("Geçersiz veri girdiniz.");
+        }
+
+    }
+}
