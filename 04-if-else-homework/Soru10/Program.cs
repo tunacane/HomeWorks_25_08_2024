e41da398a8887b71974b32928e13953afbb533d9
tunacane <tunacaneski@gmail.com>
2024-08-29 14:13:38 +0300

    soru1 tamamlandı

---
+++ 04-if-else-homework/Soru10/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Soru10;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Bir sayı giriniz: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        if (IsPrime(num1))
+        {
+            Console.WriteLine($"{num1} bir asal sayıdır.");
+        }
+        else
+        {
+            Console.WriteLine($"{num1} bir asal sayı değildir.");
+
+        }
+}
+}
---
+++ 05-switch-homework/Soru1/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Soru1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("1-7 arasında bir sayı giriniz: ");
+        int sayi = Convert.ToInt32(Console.ReadLine());
+        string day="";
+
+        switch (sayi)
+        {
+            case  1:
+                day = "Monday";
+                break;
+            case  2:
+                day ="Tuesday";
+                break;    
+            case  3:
+                day ="Wednesday";
+                break;
+            case  4:
+                day="Thursday";
+                break;    
+            case  5:
+                day ="Friday";
+                break;
+            case  6:
+                day= "Saturday;";
+                break;        
+
+            default :
+                System.Console.WriteLine("Sunday");
+                break;
+        }
+        System.Console.WriteLine($"Günlerden { day}");
+        
+    }
+}
