0c2f7d8fcfe2f4e9edda7a52dada29d875fcb552
tunacane <tunacaneski@gmail.com>
2024-08-29 18:24:56 +0300

    soru5 tamamlandı

---
+++ 05-switch-homework/Soru5/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Soru5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Bir sayı giriniz: ");
+        int sayi = Convert.ToInt32(Console.ReadLine());
+       
+        string result ="";
+
+        switch (sayi)
+        {
+            case 12:
+            case 1:
+            case 2:
+                result= "Season is Winter";
+                break;
+            case 3:
+            case 4:
+            case 5:
+                result = "Season is Spring";
+                break;
+            
+            case 6:
+            case 7:
+            case 8:
+                result = "Season is Summer";
+                break;
+            case 9:
+            case 10:
+            case 11:
+                result = "Season is Autumn";
+                break;
+
+            default:
+                result = "Invalid value!";
+                break;
+        }
+        System.Console.WriteLine(result);
+    }
+}
